cde05361cd937e5565221eb936f3944266c3ff04
HP <rkm2541997@gmail.com>
2022-08-20 00:22:23 +0530

    Delegated transfer (transferFrom) added

--- contracts/RAJToken.sol
+++ contracts/RAJToken.sol
@@ -17,6 +17,7 @@ contract RAJToken{
     // }
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     constructor(string memory _name, string memory _symbol, uint256 _initialSupply){
         balanceOf[msg.sender] = _initialSupply;
@@ -27,6 +28,8 @@ contract RAJToken{
 
     //Keep track of balances of each amount, mapping is like an associative array (Key value store)
     mapping(address => uint256) public balanceOf;
+    //Check allowance between owner and spender
+    mapping(address => mapping(address => uint256)) public allowance;
 
     //Transfer
     function transfer(address _to, uint256 _value) public returns(bool success){
@@ -38,6 +41,35 @@ contract RAJToken{
     emit Transfer(msg.sender, _to, _value);
     //Returns a boolean
     return true;
+    }
+
+
+    //approve
+    function approve(address _spender, uint256 _value) public returns(bool success){
+        //allowance
+        allowance[msg.sender][_spender] = _value;
+
+        //Approve event
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    //transferFrom
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+        //require _from has enough tokens
+        require(balanceOf[_from] >= _value, "fromAccount hasn't enough balance");
+        //require allowance is big enough
+        require(allowance[_from][msg.sender] >= _value, "No allowances");
+        //change the balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        //update the balance
+        allowance[_from][msg.sender] -=_value;
+        //Transfer event
+        emit Transfer(_from, _to, _value);
+        //returns a boolean
+        return true;
     }
 
 }
